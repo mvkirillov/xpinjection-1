ff1819bec8b8ca5f3ede308c360c6a0efac55c0d
kirillov <Mikhail.Kirillov@mvideo.ru>
2022-01-24 20:23:12 +0400

    initial commit

---
+++ src/main/kotlin/ru/mvideo/xpinjection/XpinjectionApplication.kt
@@ -0,0 +1,11 @@
+package ru.mvideo.xpinjection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XpinjectionApplication
+
+fun main(args: Array<String>) {
+    runApplication<XpinjectionApplication>(*args)
+}
---
+++ src/test/kotlin/ru/mvideo/xpinjection/XpinjectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.mvideo.xpinjection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XpinjectionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
